f54abb43c7713b6ffbf39161de0572c565de9ca1
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2022-05-14 21:10:21 +0900

    Add class jsonPlaceAPI extends RESTDataSource

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require("apollo-server");
 const axios = require("axios").default;
+const { RESTDataSource } = require("apollo-datasource-rest");
 
 const typeDefs = gql`
   type User {
@@ -23,6 +24,26 @@ const typeDefs = gql`
     posts: [Post]
   }
 `;
+
+class jsonPlaceAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://jsonplaceholder.typicode.com/";
+  }
+
+  async getUsers() {
+    const data = await this.get("/users");
+    return data;
+  }
+  async getUser(id) {
+    const data = await this.get(`/users/${id}`);
+    return data;
+  }
+  async getPosts() {
+    const data = await this.get("/posts");
+    return data;
+  }
+}
 
 const resolvers = {
   Query: {
